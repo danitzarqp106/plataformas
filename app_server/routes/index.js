30ec6dbf9f55833fd157a1464d076300da11f8ca
Ubuntu <danitzarqp106@gmail.com>
2016-10-09 18:33:16 -0500

    first commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+//var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/other');
+var ctrlReporte = require('../controllers/reportes');
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', ctrlMain.index);
+module.exports = router;*/
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/location/reporte', ctrlLocations.viewReporte);
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 module.exports = router;
